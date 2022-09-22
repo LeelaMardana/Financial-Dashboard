e9c5cd650c837321200d7c45b80fac2e63e07545
AwesemoTowa <Deankurumi@gmail.com>
2022-09-22 21:56:03 +0600

    added grid

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
