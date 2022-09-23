c3e255805f3e5438a3486015c457c2c84b79a23e
AwesemoTowa <Deankurumi@gmail.com>
2022-09-24 05:52:00 +0600

    added sliders

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
+  effect: 'flip',
+  autoplay: {
+    delay: 2500,
+  },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+});
+
+const swiper2 = new Swiper('.swiper-goals', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  autoplay: {
+    delay: 1500,
+  },
+  speed: 2000,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-goals-btn-next',
+    prevEl: '.swiper-goals-btn-prev',
   },
 });
